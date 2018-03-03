5f9d8a9327f5fa8a398cb2e323958aa9b90591bf
debadyuti0510 <36818630+debadyuti0510@users.noreply.github.com>
2018-03-03 22:33:59 +0530

    Update news_retrieve.py

--- news_retrieve.py
+++ news_retrieve.py
@@ -7,7 +7,7 @@ db1 = client.newsdb
 
 
 def search(keyWord):
-    api = NewsApiClient(api_key = 'efb410a05f3e4fb2a775bdc360509a17')
+    api = NewsApiClient(api_key = '')
     Content = api.get_everything(q = keyWord)
     db1.news.insert(Content)
    # print(Content)
